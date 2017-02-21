7a92c39ccf5b206aba2949dfee6a3acb259efc28
DevJSW <johnwasike0@gmail.com>
2017-02-21 11:25:39 +0300

    commiting changes

--- app/src/main/java/com/campusmail/campusmail/MainActivity.java
+++ app/src/main/java/com/campusmail/campusmail/MainActivity.java
@@ -31,7 +31,6 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.github.curioustechizen.ago.RelativeTimeTextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -343,25 +342,10 @@ public class MainActivity extends AppCompatActivity
                 });
 
                 mQueryComments = mDatabaseComment.orderByChild("post_key").equalTo(post_key);
-                mQueryComments.addChildEventListener(new ChildEventListener() {
+                mQueryComments.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
-                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    public void onDataChange(DataSnapshot dataSnapshot) {
                         viewHolder.mCommentCount.setText(dataSnapshot.getChildrenCount() + "");
-                    }
-
-                    @Override
-                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                    }
-
-                    @Override
-                    public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                    }
-
-                    @Override
-                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
                     }
 
                     @Override
